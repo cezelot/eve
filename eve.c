382340f0efac0924ec69aac8987fcd2f38a360ee
bhamed <bhamed@student.42antananarivo.mg>
2023-11-26 12:29:13 +0300

    feat: read keypresses from the user

--- eve.c
+++ eve.c
@@ -6,11 +6,18 @@
 /*   By: bhamed <bhamed@student.42antananarivo.mg>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 12:20:29 by bhamed            #+#    #+#             */
-/*   Updated: 2023/11/26 12:20:51 by bhamed           ###   ########.fr       */
+/*   Updated: 2023/11/26 12:28:03 by bhamed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 int	main(void)
 {
+	char	c;
+
+	while (read(STDIN_FILENO, &c, 1) == 1)
+	{
+	}
 	return (0);
 }
