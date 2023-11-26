cdeee2584a6aa1467133a5d8c97e161a500f27df
bhamed <bhamed@student.42antananarivo.mg>
2023-11-26 12:36:39 +0300

    feat: turn off echoing

--- eve.c
+++ eve.c
@@ -6,16 +6,27 @@
 /*   By: bhamed <bhamed@student.42antananarivo.mg>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 12:20:29 by bhamed            #+#    #+#             */
-/*   Updated: 2023/11/26 12:29:53 by bhamed           ###   ########.fr       */
+/*   Updated: 2023/11/26 12:34:48 by bhamed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <termios.h>
 #include <unistd.h>
+
+void	enable_raw_mode(void)
+{
+	struct termios	raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+	raw.c_lflag &= ~(ECHO);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int	main(void)
 {
 	char	c;
 
+	enable_raw_mode();
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
 	}
