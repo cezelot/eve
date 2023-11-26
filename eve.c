0a16e026ca0d8b37170e6b78fb67349edfa8cd33
bhamed <bhamed@student.42antananarivo.mg>
2023-11-26 12:50:11 +0300

    fix: disable raw mode at exit

--- eve.c
+++ eve.c
@@ -6,18 +6,28 @@
 /*   By: bhamed <bhamed@student.42antananarivo.mg>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 12:20:29 by bhamed            #+#    #+#             */
-/*   Updated: 2023/11/26 12:34:48 by bhamed           ###   ########.fr       */
+/*   Updated: 2023/11/26 12:49:03 by bhamed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+struct termios	g_orig_termios;
+
+void	disable_raw_mode(void)
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_orig_termios);
+}
 
 void	enable_raw_mode(void)
 {
 	struct termios	raw;
 
-	tcgetattr(STDIN_FILENO, &raw);
+	atexit(disable_raw_mode);
+	tcgetattr(STDIN_FILENO, &g_orig_termios);
+	raw = g_orig_termios;
 	raw.c_lflag &= ~(ECHO);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
