7de06b596ae21d0ae85ff386f911757d58bcf851
bhamed <bhamed@student.42antananarivo.mg>
2023-12-18 13:24:00 +0300

    fix(output): fix the absence of a tilde on the last line

--- sources/output.c
+++ sources/output.c
@@ -6,7 +6,7 @@
 /*   By: bhamed <bhamed@student.42antananarivo.mg>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 18:08:14 by bhamed            #+#    #+#             */
-/*   Updated: 2023/12/10 23:56:31 by bhamed           ###   ########.fr       */
+/*   Updated: 2023/12/18 13:03:16 by bhamed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ void	editor_draw_rows(t_env *env)
 	int	n;
 
 	n = 0;
-	while (n++ < env->screenrows)
-		write(STDOUT_FILENO, "~\r\n", 3);
+	while (n < env->screenrows)
+	{
+		write(STDOUT_FILENO, "~", 1);
+		if (n++ < env->screenrows - 1)
+			write(STDOUT_FILENO, "\r\n", 2);
+	}
 }
 
 void	editor_refresh_screen(t_env *env)
