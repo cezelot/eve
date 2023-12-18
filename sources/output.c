c9c2d26b7683d3922a30c44fa55111cf7fe1ff2c
bhamed <bhamed@student.42antananarivo.mg>
2023-12-18 19:54:25 +0300

    fix(output): fix cursor displayed in the middle of the screen somewhere for a split second while the terminal is drawing to the screen

--- sources/output.c
+++ sources/output.c
@@ -6,7 +6,7 @@
 /*   By: bhamed <bhamed@student.42antananarivo.mg>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 18:08:14 by bhamed            #+#    #+#             */
-/*   Updated: 2023/12/18 19:08:01 by bhamed           ###   ########.fr       */
+/*   Updated: 2023/12/18 19:52:59 by bhamed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,12 @@ void	editor_refresh_screen(t_env *env)
 
 	abuf.buf = NULL;
 	abuf.len = 0;
+	abuf_append(&abuf, "\x1b[?25l", 6);
 	abuf_append(&abuf, "\x1b[2J", 4);
 	abuf_append(&abuf, "\x1b[H", 3);
 	editor_draw_rows(&abuf, env);
 	abuf_append(&abuf, "\x1b[H", 3);
+	abuf_append(&abuf, "\x1b[?25h", 6);
 	write(STDOUT_FILENO, abuf.buf, abuf.len);
 	abuf_free(&abuf);
 }
