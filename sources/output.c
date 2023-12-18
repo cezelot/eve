bf999aac471b37179f5c6e7998045a56be5d8222
bhamed <bhamed@student.42antananarivo.mg>
2023-12-18 20:07:16 +0300

    perf(output): clear each line as we redraw them instead of clearing the entire screen before each refresh

--- sources/output.c
+++ sources/output.c
@@ -6,7 +6,7 @@
 /*   By: bhamed <bhamed@student.42antananarivo.mg>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 18:08:14 by bhamed            #+#    #+#             */
-/*   Updated: 2023/12/18 19:52:59 by bhamed           ###   ########.fr       */
+/*   Updated: 2023/12/18 20:01:21 by bhamed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	editor_draw_rows(t_abuf *abuf, t_env *env)
 	while (n < env->screenrows)
 	{
 		abuf_append(abuf, "~", 1);
+		abuf_append(abuf, "\x1b[K", 3);
 		if (n++ < env->screenrows - 1)
 			abuf_append(abuf, "\r\n", 2);
 	}
@@ -35,7 +36,6 @@ void	editor_refresh_screen(t_env *env)
 	abuf.buf = NULL;
 	abuf.len = 0;
 	abuf_append(&abuf, "\x1b[?25l", 6);
-	abuf_append(&abuf, "\x1b[2J", 4);
 	abuf_append(&abuf, "\x1b[H", 3);
 	editor_draw_rows(&abuf, env);
 	abuf_append(&abuf, "\x1b[H", 3);
