7f3f28e1a415ddad0ac3b5b0c6508cf342700d55
cezelot <cezelot@proton.me>
2024-05-28 16:39:22 +0300

    feat(input): scroll up or down an entire page when 'Page Up' or 'Page Down' key is pressed

--- sources/input/input.c
+++ sources/input/input.c
@@ -4,7 +4,7 @@
 /*   by: cezelot <cezelot@proton.me>                               d8P'88P    */
 /*                                                                d8P         */
 /*   Created: 2023/11/27 18:32:33 by cezelot                     d8P.a8P      */
-/*   Updated: 2024/05/28 10:30:17 by cezelot                     d888P'       */
+/*   Updated: 2024/05/28 16:33:09 by cezelot                     d888P'       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,25 +31,36 @@ static void	editor_move_cursor(t_env *env, int key)
 	snap_cursor_to_end_of_line(env, row, rowlen);
 }
 
-static void	process_esc_seq_keys(int c, t_env *env)
+static void	process_page_keys(t_env *env, int c)
 {
 	int	times;
 
 	times = env->screenrows;
+	if (c == PAGE_UP)
+		env->cy = env->rowoff;
+	else if (c == PAGE_DOWN)
+	{
+		env->cy = env->rowoff + env->screenrows - 1;
+		if (env->cy > env->numrows)
+			env->cy = env->numrows;
+	}
+	while (times--)
+	{
+		if (c == PAGE_UP)
+			editor_move_cursor(env, ARROW_UP);
+		else
+			editor_move_cursor(env, ARROW_DOWN);
+	}
+}
+
+static void	process_esc_seq_keys(int c, t_env *env)
+{
 	if (c == HOME_KEY)
 		env->cx = 0;
 	else if (c == END_KEY)
 		env->cx = env->screencols - 1;
 	else if ((c == PAGE_UP) || (c == PAGE_DOWN))
-	{
-		while (times--)
-		{
-			if (c == PAGE_UP)
-				editor_move_cursor(env, ARROW_UP);
-			else
-				editor_move_cursor(env, ARROW_DOWN);
-		}
-	}
+		process_page_keys(env, c);
 	else if ((c == ARROW_LEFT) || (c == ARROW_RIGHT) \
 			|| (c == ARROW_UP) || (c == ARROW_DOWN))
 		editor_move_cursor(env, c);
